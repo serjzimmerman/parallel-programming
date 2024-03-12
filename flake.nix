e723ab2708282fee3b67f1edb3073fca0b98141c
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2024-03-12 21:23:32 +0300

    flake: switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,52 +1,30 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    lint-nix.url = "github:xc-jp/lint.nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ inputs: let
-    systems = ["x86_64-linux" "aarch64-linux"];
-  in
-    flake-utils.lib.eachSystem systems (system: let
-      pkgs = import nixpkgs {inherit system;};
+  outputs =
+    { flake-parts, treefmt-nix, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
 
-      clang-tools = pkgs.clang-tools_17;
-      lints = (import ./nix/lints.nix {inherit inputs;}) pkgs ./.;
-      nativeBuildInputs = with pkgs; [
-        clang-tools
-        act
-        just
-        mpi
-        automake
-        autoconf
-        addlicense
-        gdb
-        gnuplot
-        bc
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
       ];
 
-      shell =
-        (pkgs.mkShell.override {
-          stdenv = pkgs.gcc49Stdenv;
-        }) {
-          inherit nativeBuildInputs;
+      perSystem =
+        { ... }:
+        {
+          imports = [
+            ./nix/treefmt.nix
+            ./nix/persystem.nix
+          ];
         };
-    in {
-      devShells = {
-        default = shell;
-        devGcc = shell;
-      };
-
-      legacyPackages =
-        lints
-        // {};
-
-      formatter = pkgs.alejandra;
-    });
+    };
 }
---
+++ nix/treefmt.nix
@@ -0,0 +1,14 @@
+{
+  treefmt = {
+    projectRootFile = "flake.nix";
+    programs = {
+      nixfmt-rfc-style.enable = true;
+      yamlfmt.enable = true;
+      deadnix.enable = true;
+      just.enable = true;
+      keep-sorted.enable = true;
+      mdformat.enable = true;
+      clang-format.enable = true;
+    };
+  };
+}
