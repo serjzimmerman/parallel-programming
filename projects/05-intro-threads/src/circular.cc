3060ada0552cc6722bb9fd8ae711472747cc985b
Sergei Zimmerman <sstsimmerman@gmail.com>
2024-04-30 23:30:43 +0300

    feat(threads-intro): add simple sequential latches program

---
+++ projects/05-intro-threads/src/circular.cc
@@ -0,0 +1,89 @@
+// Copyright (c) 2024 Sergei Zimmerman
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#include <boost/program_options.hpp>
+#include <fmt/core.h>
+#include <range/v3/all.hpp>
+
+#include <barrier>
+#include <iostream>
+#include <latch>
+#include <map>
+#include <mutex>
+#include <thread>
+
+namespace po = boost::program_options;
+
+auto main(int argc, char **argv) -> int {
+  auto desc = po::options_description{"allowed options"};
+
+  desc.add_options()("help,h", "produce this help message")(
+      "num-threads,n", po::value<uint32_t>()->default_value(8),
+      "number of threads to use");
+
+  auto vm = po::variables_map{};
+  po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return EXIT_FAILURE;
+  }
+
+  auto thread_id = uint32_t{0};
+  auto count = vm.at("num-threads").as<uint32_t>();
+  auto barrier = std::barrier{count};
+
+  auto shared_variable = 0;
+  auto mutex = std::mutex{};
+
+  auto threads = std::vector<std::jthread>{};
+  auto latches = std::map<uint32_t, std::latch>{};
+
+  auto create_handler = [&](auto i) {
+    return [&, i] {
+      barrier.arrive_and_wait();
+
+      auto &latch = latches.at(i);
+      latch.wait();
+
+      {
+        auto lock = std::unique_lock{mutex};
+        fmt::println("thread-id = {}, value = {}", i, shared_variable++);
+      }
+
+      if (i != count - 1) {
+        auto &next_latch = latches.at(i + 1);
+        next_latch.count_down();
+      }
+    };
+  };
+
+  for (auto i : ranges::views::iota(thread_id + 1, count)) {
+    latches.emplace(std::piecewise_construct, std::forward_as_tuple(i),
+                    std::forward_as_tuple(1));
+    threads.push_back(std::jthread{create_handler(i)});
+  }
+
+  latches.emplace(std::piecewise_construct, std::forward_as_tuple(0),
+                  std::forward_as_tuple(0));
+  create_handler(thread_id)();
+}
